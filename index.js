5f181eff1a78738c58415f2750ebb02cba4ce134
jiaowochunge <wangyi8888@gmail.com>
2021-03-26 17:21:40 +0800

    read database params from cmd line

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const { getCachedResult, storeCache } = require('./simpleCache')
 const express = require('express')
+const mysql = require('mysql')
+const argv = require('minimist')(process.argv.slice(2))
+
+const { u: user, p: password, database } = argv
+if (!user || !password) {
+	console.log('usage: node index.js -u "username" -p "password" -database "dbname"')
+	process.exit(1)
+}
+
 const app = express()
 const port = 3000
 
 const statSQL = `select count(*) as count, mins from (select concat(date_format(created_at,'%Y-%m-%d %H:'), rpad(floor(minute(created_at) / 10) * 10, 2, 0)) as mins from fazhi_toupiao where vote_content like ?) as t group by mins;`
 
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Siren123!',
-  database: 'wx_test'
-})
-connection.connect()
+let connection = null
+function handleConnect() {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: user,
+		password: password,
+		database: database || 'wx_test'
+	})
+	connection.connect()
+
+	connection.on('error', function(err) {
+    console.log('db error', err)
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleConnect()
+    } else {
+      throw err
+    }
+  })
+}
+handleConnect()
 
 app.use(express.static('public'))
 
